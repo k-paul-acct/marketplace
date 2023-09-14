ad8b423212e31151ad8a6ce98f84ba349f64f4ed
legen <legenadary.pigeon@gmail.com>
2023-09-14 21:59:00 +0300

    Generic API Handler added

---
+++ API Handlers/APIHandler.cs
@@ -0,0 +1,155 @@
+﻿using API_Marketplace_.net_7_v1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace API_Marketplace_.net_7_v1.API_Handlers
+{
+    public class APIHandler
+    {
+        public static async Task CreateEntityAsync<T>(HttpContext context, MarketplaceDbContext dbContext) where T : class
+        {
+            // Читаем JSON-тело запроса
+            using var reader = new StreamReader(context.Request.Body);
+            var jsonBody = await reader.ReadToEndAsync();
+
+            try
+            {
+                // Десериализуем JSON в объект типа T
+                var newEntity = JsonSerializer.Deserialize<T>(jsonBody);
+
+                // Добавляем объект в DbSet<T> и сохраняем изменения в базе данных
+                dbContext.Set<T>().Add(newEntity);
+                await dbContext.SaveChangesAsync();
+
+                context.Response.StatusCode = 201; // Created
+                await context.Response.WriteAsync($"{typeof(T).Name} created successfully.");
+            }
+            catch (JsonException)
+            {
+                context.Response.StatusCode = 400; // Bad Request
+                await context.Response.WriteAsync("Invalid JSON data.");
+            }
+        }
+
+        public static async Task GetEntityAsync<T>(HttpContext context, MarketplaceDbContext dbContext) where T : class
+        {
+            if (context.Request.RouteValues["EntityId"] is string entityIdStr && int.TryParse(entityIdStr, out int entityId))
+            {
+                // Найдите объект типа T в базе данных по entityId
+                var entity = await dbContext.Set<T>().FindAsync(entityId);
+
+                if (entity != null)
+                {
+                    // Сериализуйте объект типа T в JSON и верните его клиенту
+                    var entityJson = JsonSerializer.Serialize(entity);
+                    context.Response.StatusCode = 200; // OK
+                    context.Response.ContentType = "application/json";
+                    await context.Response.WriteAsJsonAsync(entityJson);
+                }
+                else
+                {
+                    context.Response.StatusCode = 404; // Not Found
+                    await context.Response.WriteAsync($"{typeof(T).Name} not found.");
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 400; // Bad Request
+                await context.Response.WriteAsync($"Invalid {typeof(T).Name} ID format.");
+            }
+        }
+
+        public static async Task GetAllEntitiesAsync<T>(MarketplaceDbContext dbContext, HttpContext context) where T : class
+        {
+            var entityJson = JsonSerializer.Serialize(await dbContext.Set<T>().ToListAsync());
+            await context.Response.WriteAsJsonAsync(entityJson);
+        }
+
+        public static async Task UpdateEntityAsync<T>(HttpContext context, MarketplaceDbContext dbContext) where T : class
+        {
+            // Получите EntityId из URL
+            if (context.Request.RouteValues["Id"] is string entityIdStr && int.TryParse(entityIdStr, out int entityId))
+            {
+                // Читаем JSON-тело запроса
+                using var reader = new StreamReader(context.Request.Body);
+                var jsonBody = await reader.ReadToEndAsync();
+
+                try
+                {
+                    // Десериализуем JSON в объект типа T и обновляем данные объекта
+                    var entityToUpdate = await dbContext.Set<T>().FindAsync(entityId);
+                    if (entityToUpdate != null)
+                    {
+                        var updatedEntity = JsonSerializer.Deserialize<T>(jsonBody);
+
+                        // Модифицируем объект типа T, заменяя только не null значения полей
+                        foreach (var propertyInfo in typeof(T).GetProperties())
+                        {
+                            // Исключаем поле с именем "Id" (или другое поле, которое не должно изменяться)
+                            if (!(propertyInfo.Name.Contains("ID") || propertyInfo.Name.Contains("Id")))
+                            {
+                                var newValue = propertyInfo.GetValue(updatedEntity);
+                                if (newValue != null)
+                                {
+                                    propertyInfo.SetValue(entityToUpdate, newValue);
+                                }
+                            }
+                        }
+
+
+                        // Сохраняем изменения в базе данных
+                        await dbContext.SaveChangesAsync();
+                        context.Response.StatusCode = 200; // OK
+                        await context.Response.WriteAsync($"{typeof(T).Name} updated successfully.");
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 404; // Not Found
+                        await context.Response.WriteAsync($"{typeof(T).Name} not found.");
+                    }
+                }
+                catch (JsonException)
+                {
+                    // Ошибка в формате JSON-данных, возвращаем ошибку
+                    context.Response.StatusCode = 400; // Bad Request
+                    await context.Response.WriteAsync("Invalid JSON data.");
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 400; // Bad Request
+                await context.Response.WriteAsync($"Invalid {typeof(T).Name} ID format.");
+            }
+        }
+
+
+        public static async Task DeleteEntityByIDAsync<T>(HttpContext context, MarketplaceDbContext dbContext) where T : class
+        {
+            if (context.Request.RouteValues["EntityId"] is string entityIdStr && int.TryParse(entityIdStr, out int entityId))
+            {
+                // Найдите объект типа T в базе данных по entityId
+                var entityToDelete = await dbContext.Set<T>().FindAsync(entityId);
+
+                if (entityToDelete != null)
+                {
+                    // Удаляем объект из DbSet<T> и сохраняем изменения
+                    dbContext.Set<T>().Remove(entityToDelete);
+                    await dbContext.SaveChangesAsync();
+                    context.Response.StatusCode = 200; // OK
+                    await context.Response.WriteAsync($"{typeof(T).Name} deleted successfully.");
+                }
+                else
+                {
+                    context.Response.StatusCode = 404; // Not Found
+                    await context.Response.WriteAsync($"{typeof(T).Name} not found.");
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 400; // Bad Request
+                await context.Response.WriteAsync($"Invalid {typeof(T).Name} ID format.");
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using API_Marketplace_.net_7_v1.API_Handlers;
 using API_Marketplace_.net_7_v1.Controllers;
 using API_Marketplace_.net_7_v1.Models;
 using Microsoft.AspNetCore;
@@ -24,139 +25,140 @@ var app = builder.Build();
 
 // Users
 app.MapPut("/api/user/create", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await UserAPIHandler.CreateUserAsync(context, dbContext));
+    await APIHandler.CreateEntityAsync<User>(context, dbContext));
 
 app.MapDelete("/api/user/deletebyid/{userId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await UserAPIHandler.DeleteUserByIDAsync(context, dbContext));
+    await APIHandler.DeleteEntityByIDAsync<User>(context, dbContext));
 
 app.MapGet("/api/user/getbyid/{userId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await UserAPIHandler.GetUserByIDAsync(context, dbContext));
+    await APIHandler.GetEntityAsync<User>(context, dbContext));
 
-app.MapPost("/api/user/updatebyid/{userId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await UserAPIHandler.UpdateUserAsync(context, dbContext));
+app.MapPost("/api/user/updatebyid/{Id}", async (MarketplaceDbContext dbContext, HttpContext context) =>
+    await APIHandler.UpdateEntityAsync<User>(context, dbContext));
 
-app.MapGet("/api/user/getall", async (HttpContext context) =>
-    await context.Response.WriteAsJsonAsync(await UserAPIHandler.GetAllUsersAsync()));
+app.MapGet("/api/user/getall", async (MarketplaceDbContext dbContext, HttpContext context) =>
+    await APIHandler.GetAllEntitiesAsync<User>(dbContext, context));
 
-app.MapPost("/api/user/auth", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await context.Response.WriteAsJsonAsync(await UserAPIHandler.AuthenticateUserAsync(context, dbContext)));
 
 
 // Products
 app.MapPut("/api/product/create", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await ProductAPIHandler.CreateProductAsync(context, dbContext));
+    await APIHandler.CreateEntityAsync<Product>(context, dbContext));
 
 app.MapDelete("/api/product/deletebyid/{productId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await ProductAPIHandler.DeleteProductByIDAsync(context, dbContext));
+    await APIHandler.DeleteEntityByIDAsync<Product>(context, dbContext));
 
 app.MapGet("/api/product/getbyid/{productId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await ProductAPIHandler.GetProductAsync(context, dbContext));
+    await APIHandler.GetEntityAsync<Product>(context, dbContext));
 
 app.MapPost("/api/product/updatebyid/{productId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await ProductAPIHandler.UpdateProductAsync(context, dbContext));
+    await APIHandler.UpdateEntityAsync<Product>(context, dbContext));
 
-app.MapGet("/api/product/getall", async (HttpContext context) =>
-    await context.Response.WriteAsJsonAsync(await ProductAPIHandler.GetAllProductsAsync()));
+app.MapGet("/api/product/getall", async (MarketplaceDbContext dbContext, HttpContext context) =>
+    await APIHandler.GetAllEntitiesAsync<Product>(dbContext, context));
 
 
-//Orders
+// Orders
 app.MapPut("/api/order/create", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await OrderAPIHandler.CreateOrderAsync(context, dbContext));
+    await APIHandler.CreateEntityAsync<Order>(context, dbContext));
 
 app.MapDelete("/api/order/deletebyid/{orderId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await OrderAPIHandler.CreateOrderAsync(context, dbContext));
+    await APIHandler.DeleteEntityByIDAsync<Order>(context, dbContext));
 
 app.MapGet("/api/order/getbyid/{orderId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await OrderAPIHandler.CreateOrderAsync(context, dbContext));
+    await APIHandler.GetEntityAsync<Order>(context, dbContext));
 
 app.MapPost("/api/order/updatebyid/{orderId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await OrderAPIHandler.CreateOrderAsync(context, dbContext));
-app.MapGet("/api/order/getall", async (HttpContext context) =>
-    await context.Response.WriteAsJsonAsync(await OrderAPIHandler.GetAllOrdersAsync()));
+    await APIHandler.UpdateEntityAsync<Order>(context, dbContext));
+
+app.MapGet("/api/order/getall", async (MarketplaceDbContext dbContext, HttpContext context) =>
+    await APIHandler.GetAllEntitiesAsync<Order>(dbContext, context));
 
 
 // OrderItems
 app.MapPut("/api/orderitem/create", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await OrderItemAPIHandler.CreateOrderItemAsync(context, dbContext));
+    await APIHandler.CreateEntityAsync<OrderItem>(context, dbContext));
 
 app.MapDelete("/api/orderitem/deletebyid/{orderItemId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await OrderItemAPIHandler.DeleteOrderItemByIdAsync(context, dbContext));
+    await APIHandler.DeleteEntityByIDAsync<OrderItem>(context, dbContext));
 
 app.MapGet("/api/orderitem/getbyid/{orderItemId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await OrderItemAPIHandler.GetOrderItemAsync(context, dbContext));
+    await APIHandler.GetEntityAsync<OrderItem>(context, dbContext));
 
 app.MapPost("/api/orderitem/updatebyid/{orderItemId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await OrderItemAPIHandler.UpdateOrderItemAsync(context, dbContext));
+    await APIHandler.UpdateEntityAsync<OrderItem>(context, dbContext));
 
-app.MapGet("/api/orderitem/getall", async (HttpContext context) =>
-    await context.Response.WriteAsJsonAsync(await OrderItemAPIHandler.GetAllOrderItemsAsync()));
+app.MapGet("/api/orderitem/getall", async (MarketplaceDbContext dbContext, HttpContext context) =>
+    await APIHandler.GetAllEntitiesAsync<OrderItem>(dbContext, context));
 
 
 // Reviews
 app.MapPut("/api/review/create", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await ReviewAPIHandler.CreateReviewAsync(context, dbContext));
+    await APIHandler.CreateEntityAsync<Review>(context, dbContext));
 
 app.MapDelete("/api/review/deletebyid/{reviewId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await ReviewAPIHandler.DeleteReviewByIdAsync(context, dbContext));
+    await APIHandler.DeleteEntityByIDAsync<Review>(context, dbContext));
 
 app.MapGet("/api/review/getbyid/{reviewId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await ReviewAPIHandler.GetReviewAsync(context, dbContext));
+    await APIHandler.GetEntityAsync<Review>(context, dbContext));
 
 app.MapPost("/api/review/updatebyid/{reviewId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await ReviewAPIHandler.UpdateReviewAsync(context, dbContext));
+    await APIHandler.UpdateEntityAsync<Review>(context, dbContext));
 
-app.MapGet("/api/review/getall", async (HttpContext context) =>
-    await context.Response.WriteAsJsonAsync(await ReviewAPIHandler.GetAllReviewsAsync()));
+app.MapGet("/api/review/getall", async (MarketplaceDbContext dbContext, HttpContext context) =>
+    await APIHandler.GetAllEntitiesAsync<Review>(dbContext, context));
 
 
 // Categories
 app.MapPut("/api/category/create", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await CategoryAPIHandler.CreateCategoryAsync(context, dbContext));
+    await APIHandler.CreateEntityAsync<Category>(context, dbContext));
 
 app.MapDelete("/api/category/getbyid/{categoryId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await CategoryAPIHandler.GetCategoryAsync(context, dbContext));
+    await APIHandler.DeleteEntityByIDAsync<Category>(context, dbContext));
 
 app.MapPost("/api/category/updatebyid/{categoryId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await CategoryAPIHandler.UpdateCategoryAsync(context, dbContext));
+    await APIHandler.UpdateEntityAsync<Category>(context, dbContext));
 
 app.MapGet("/api/category/deletebyid/{categoryId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await CategoryAPIHandler.DeleteCategoryByIDAsync(context, dbContext));
+    await APIHandler.GetEntityAsync<Category>(context, dbContext));
 
-app.MapGet("/api/category/getall", async (HttpContext context) =>
-    await context.Response.WriteAsJsonAsync(await CategoryAPIHandler.GetAllCategoriesAsync()));
+app.MapGet("/api/category/getall", async (MarketplaceDbContext dbContext, HttpContext context) =>
+    await APIHandler.GetAllEntitiesAsync<Category>(dbContext, context));
+
 
 // Wishlists
 app.MapPut("/api/wishlist/create", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await WishlistAPIHandler.CreateWishlistAsync(context, dbContext));
+    await APIHandler.CreateEntityAsync<Wishlist>(context, dbContext));
 
 app.MapDelete("/api/wishlist/getbyid/{wishlistId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await WishlistAPIHandler.GetWishlistAsync(context, dbContext));
+    await APIHandler.DeleteEntityByIDAsync<Wishlist>(context, dbContext));
 
 app.MapPost("/api/wishlist/updatebyid/{wishlistId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await WishlistAPIHandler.UpdateWishlistAsync(context, dbContext));
+    await APIHandler.UpdateEntityAsync<Wishlist>(context, dbContext));
 
 app.MapGet("/api/wishlist/deletebyid/{wishlistId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await WishlistAPIHandler.DeleteWishlistByIDAsync(context, dbContext));
+    await APIHandler.GetEntityAsync<Wishlist>(context, dbContext));
 
-app.MapGet("/api/wishlist/getall", async (HttpContext context) =>
-    await context.Response.WriteAsJsonAsync(await WishlistAPIHandler.GetAllWishlistsAsync()));
+app.MapGet("/api/wishlist/getall", async (MarketplaceDbContext dbContext, HttpContext context) =>
+    await APIHandler.GetAllEntitiesAsync<Wishlist>(dbContext, context));
 
 
 // Roles
 app.MapPut("/api/role/create", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await RoleAPIHandler.CreateRoleAsync(context, dbContext));
+    await APIHandler.CreateEntityAsync<Role>(context, dbContext));
 
 app.MapDelete("/api/role/getbyid/{roleId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await RoleAPIHandler.GetRoleAsync(context, dbContext));
+    await APIHandler.DeleteEntityByIDAsync<Role>(context, dbContext));
 
 app.MapPost("/api/role/updatebyid/{roleId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await RoleAPIHandler.UpdateRoleAsync(context, dbContext));
+    await APIHandler.UpdateEntityAsync<Role>(context, dbContext));
 
 app.MapGet("/api/role/deletebyid/{roleId}", async (MarketplaceDbContext dbContext, HttpContext context) =>
-    await RoleAPIHandler.DeleteRoleByIDAsync(context, dbContext));
+    await APIHandler.GetEntityAsync<Role>(context, dbContext));
 
-app.MapGet("/api/role/getall", async (HttpContext context) =>
-    await context.Response.WriteAsJsonAsync(await RoleAPIHandler.GetAllRolesAsync()));
+app.MapGet("/api/role/getall", async (MarketplaceDbContext dbContext, HttpContext context) =>
+    await APIHandler.GetAllEntitiesAsync<Role>(dbContext, context));
+
 
 
 app.Run();
