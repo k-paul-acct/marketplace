bc19d2ad7c027ad934e7aa8f65d188e7df288bae
Pavel Kulakov <k.paul19.acc@gmail.com>
2023-10-12 18:11:39 +0300

    DB removal removed

--- Marketplace.Api/Program.cs
+++ Marketplace.Api/Program.cs
@@ -137,7 +137,6 @@ app.MapGet("/api/role/getall", async (MarketplaceDbContext dbContext, HttpContex
 // DB stuff.
 using var scope = app.Services.CreateScope();
 await using var dbContext = scope.ServiceProvider.GetRequiredService<MarketplaceDbContext>();
-await dbContext.Database.EnsureDeletedAsync();
 await dbContext.Database.MigrateAsync();
 
 app.Run();
